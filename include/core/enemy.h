67db34b7f55dc3e154825d119ccfff40f255161e
jlee <lee0916jh@gmail.com>
2020-11-14 01:26:43 -0600

    Added Enemy class and it will approach tank

--- include/core/enemy.h
+++ include/core/enemy.h
@@ -1,11 +1,23 @@
 #ifndef TANK_HERO_ENEMY_H
 #define TANK_HERO_ENEMY_H
 
-namespace  tank_hero {
+#include "cinder/gl/gl.h"
+#include "core/tank.h"
+
+namespace tank_hero {
+using glm::vec2;
+
 class Enemy {
  public:
-  Enemy();
+  Enemy(vec2 position, float speed, Tank* target);
+
+  void ApproachTank();
+
  private:
+
+  vec2 position_;
+  float speed_;
+  Tank* target_;
 };
-}
+}  // namespace tank_hero
 #endif  // TANK_HERO_ENEMY_H
--- src/core/enemy.cpp
+++ src/core/enemy.cpp
@@ -1,5 +1,12 @@
-//
-// Created by Jun Lee on 11/14/20.
-//
-
 #include "core/enemy.h"
+
+namespace tank_hero {
+
+Enemy::Enemy(vec2 position, float speed, Tank* target)
+    : position_(position), speed_(speed), target_(target) {}
+
+void Enemy::ApproachTank() {
+  vec2 direction = glm::normalize(target_->GetPosition() - position_);
+  position_ += speed_ * direction;
+}
+}  // namespace tank_hero
