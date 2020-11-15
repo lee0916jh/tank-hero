c8f91ded4008e3654961d3fb0f96181ca48aa548
jlee <lee0916jh@gmail.com>
2020-11-15 01:54:09 -0600

    Added documentation on enemy.h and test cases for Enemy class

--- include/core/enemy.h
+++ include/core/enemy.h
@@ -10,22 +10,25 @@ using glm::vec2;
 
 class Enemy {
  public:
+  /// Constructor
+  /// \param position Spawn position
+  /// \param target Target to chase
   Enemy(const vec2& position, float speed, Tank* target);
 
-  // Getters
+  // Getters and Setters
   const vec2& GetPosition() const { return position_; }
   float GetRadius() const { return radius_; }
   bool IsDead() const { return dead_; }
-
-  void ApproachTank();
   void Dies() { dead_ = true; }
+
+  /// The Enemy moves towards the target
+  void ApproachTank();
 
  private:
   vec2 position_;
   float radius_ = kEnemyRadius;
   float speed_;
   Tank* target_;
-
   bool dead_ = false;
 };
 }  // namespace tank_hero
---
+++ tests/enemy_test.cpp
@@ -0,0 +1,33 @@
+#include "core/enemy.h"
+
+#include "catch2/catch.hpp"
+#include "cinder/gl/gl.h"
+#include "core/tank.h"
+
+using glm::vec2;
+using tank_hero::Enemy;
+using tank_hero::Tank;
+
+TEST_CASE("Enemy approaches the tank","[ApproachTank]") {
+  Tank tank(vec2(10, 10));
+  SECTION("Approach from the right") {
+    Enemy enemy(vec2(15, 10), 1, &tank);
+    enemy.ApproachTank();
+    REQUIRE(enemy.GetPosition() == vec2(14, 10));
+  }
+  SECTION("Approach from the left") {
+    Enemy enemy(vec2(5, 10), 1, &tank);
+    enemy.ApproachTank();
+    REQUIRE(enemy.GetPosition() == vec2(6, 10));
+  }
+  SECTION("Approach from the top") {
+    Enemy enemy(vec2(10, 0), 5, &tank);
+    enemy.ApproachTank();
+    REQUIRE(enemy.GetPosition() == vec2(10, 5));
+  }
+  SECTION("Approach from the bottom") {
+    Enemy enemy(vec2(10, 20), 1, &tank);
+    enemy.ApproachTank();
+    REQUIRE(enemy.GetPosition() == vec2(10, 19));
+  }
+}
